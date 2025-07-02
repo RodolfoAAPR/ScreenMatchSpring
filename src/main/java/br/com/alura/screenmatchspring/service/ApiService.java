d2462fbe6e3c226ba6ee0cfa9ea53ee5f6bbff3e
RodolfoAAPR <rodolfoaapr.tech@gmail.com>
2025-07-02 17:54:17 -0300

    refactor(ApiService): refactor class name

--- src/main/java/br/com/alura/screenmatchspring/service/ApiService.java
+++ src/main/java/br/com/alura/screenmatchspring/service/ApiService.java
@@ -6,7 +6,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class ConsumoApi {
+public class ApiService {
     public String getData(String endereco){
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
