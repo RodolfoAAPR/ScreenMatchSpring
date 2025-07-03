aec34526108ac45d65293b2fa7ac912ec9431da8
RodolfoAAPR <rodolfoaapr.tech@gmail.com>
2025-07-03 17:52:51 -0300

    feat(Episode): add constructor

--- src/main/java/br/com/alura/screenmatchspring/model/Episode.java
+++ src/main/java/br/com/alura/screenmatchspring/model/Episode.java
@@ -1,13 +1,23 @@
 package br.com.alura.screenmatchspring.model;
 
+import org.springframework.cglib.core.Local;
+
 import java.time.LocalDate;
 
 public class Episode {
     private Integer season;
     private String title;
     private Integer episodeNumber;
-    private Double imdbRating;
+    private Double rating;
     private LocalDate releaseDate;
+
+    public Episode(Integer seasonNumber, EpisodesData episodesData){
+        this.season = seasonNumber;
+        this.title = episodesData.title();
+        this.episodeNumber = episodesData.episodeNumber();
+        this.rating = Double.valueOf(episodesData.imdbRating());
+        this.releaseDate = LocalDate.parse(episodesData.releaseDate());
+    }
 
     public Integer getSeason() {
         return season;
@@ -34,11 +44,11 @@ public class Episode {
     }
 
     public Double getImdbRating() {
-        return imdbRating;
+        return rating;
     }
 
     public void setImdbRating(Double imdbRating) {
-        this.imdbRating = imdbRating;
+        this.rating = imdbRating;
     }
 
     public LocalDate getReleaseDate() {
