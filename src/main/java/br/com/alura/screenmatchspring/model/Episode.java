678d7b5411906eaa360aa96eea0dc5ee45611502
RodolfoAAPR <rodolfoaapr.tech@gmail.com>
2025-07-03 17:40:31 -0300

    feat(Episode): add class episode

---
+++ src/main/java/br/com/alura/screenmatchspring/model/Episode.java
@@ -0,0 +1,51 @@
+package br.com.alura.screenmatchspring.model;
+
+import java.time.LocalDate;
+
+public class Episode {
+    private Integer season;
+    private String title;
+    private Integer episodeNumber;
+    private Double imdbRating;
+    private LocalDate releaseDate;
+
+    public Integer getSeason() {
+        return season;
+    }
+
+    public void setSeason(Integer season) {
+        this.season = season;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getEpisodeNumber() {
+        return episodeNumber;
+    }
+
+    public void setEpisodeNumber(Integer episodeNumber) {
+        this.episodeNumber = episodeNumber;
+    }
+
+    public Double getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(Double imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
